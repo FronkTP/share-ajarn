b9925083752d451ac4297c06bad5a97366087d60
FronkTP <96272954+FronkTP@users.noreply.github.com>
2025-05-15 03:03:11 +0700

    remove unused test component

--- frontend/screens/ProfessorListScreen.jsx
+++ frontend/screens/ProfessorListScreen.jsx
@@ -42,7 +42,6 @@ const renderItem = ({ item }) => (
 
   return (
     <View style={styles.container}>
-      <Test/>
       <Text style={styles.title}>Professor List</Text>
       <FlatList
         data={professors}
